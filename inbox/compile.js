82ae1cc7bfe5eeb2a63bc37b8f79ce31e5747130
sanjibbaral435 <sanjibbaral.nit@gmail.com>
2019-01-17 16:07:57 -0600

    Added basic contract code and test case

--- inbox/compile.js
+++ inbox/compile.js
@@ -6,3 +6,4 @@ const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
 module.exports = solc.compile(source, 1).contracts[':Inbox'];
+//console.log(solc.compile(source, 1));
